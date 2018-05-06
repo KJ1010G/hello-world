ba193e350b7d25fb43fa7b953d7bb4fc76b2e3ed
KJ1010G <38991076+KJ1010G@users.noreply.github.com>
2018-05-06 14:49:19 +0530

    Update WhoIsWhat.java

--- java_practice/WhoIsWhat.java
+++ java_practice/WhoIsWhat.java
@@ -1,6 +1,5 @@
 public class WhoIsWhat {
 	public static void main (String[] args) {
-		String temp;
 		String[] name={"Marnoris","Zyhmet","Vana","KJ","Nobody","Everybody"};
 		String[] des={"dead","the best","good with babes","less coward than goldie","sneaky","hungry"};
 		
